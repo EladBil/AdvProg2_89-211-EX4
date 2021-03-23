ad0042ef6614dd454cf9c5e5f83f1f5544c1ad1e
Yedidya Bachar <72942868+yedidya3@users.noreply.github.com>
2021-03-23 17:41:50 +0200

    model
    version 0 to model

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+
+namespace model
+{
+    interface Model : INotifyPropertyChanged
+    {
+        
+        //Frame getFrame();
+        void setIndexFrame(int index);
+        int getIndexFrame();
+        int getNumberRows();
+        float getRefreshRate();
+        void setRefreshRate(float speed);
+        /*
+         * get most cor receives a field (thats on a csv) and returns which one of the other fields are the most corralitive from the 
+         * begining to end based
+         * on the pearson.
+         * */
+        string getMostCor(string givenIndex);
+
+        //Line getRegres(
+
+
+
+
+
+
+    }
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
