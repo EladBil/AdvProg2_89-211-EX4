9115c93628f4bf091d65f70408e146fe8e7fef30
shmuelyaish <73137368+shmuelyaish@users.noreply.github.com>
2021-03-29 09:14:10 +0300

    Add files via upload

---
+++ libraries.h
@@ -0,0 +1,53 @@
+#pragma once
+#include "timeseries.h"
+#include "HybridAnomalyDetector.h"
+/*
+* TimeSeries
+*/
+extern "C" __declspec(dllexport) void* createTS(const char* CSVfileName) {
+	return (void*) new TimeSeries(CSVfileName);
+}
+
+extern "C" __declspec(dllexport) vector<float> TsGetAttributeData(TimeSeries * ts, string name) {
+	return ts->getAttributeData(name);
+}
+extern "C" __declspec(dllexport) vector<string> TsgettAttributes(TimeSeries * ts) {
+	return ts->gettAttributes();
+}
+extern "C" __declspec(dllexport) size_t TsgetRowSize(TimeSeries * ts) {
+	return ts->getRowSize();
+}
+
+/*
+* SimpleAnomalyDetector
+*/
+extern "C" __declspec(dllexport) void* createSad() {
+	return (void*) new SimpleAnomalyDetector();
+}
+extern "C" __declspec(dllexport) void SadLearnNormal(SimpleAnomalyDetector * s, const TimeSeries & ts) {
+	s->learnNormal(ts);
+}
+extern "C" __declspec(dllexport)  vector<AnomalyReport> SadDetect(SimpleAnomalyDetector * s, const TimeSeries & ts) {
+	return s->detect(ts);
+}
+extern "C" __declspec(dllexport) vector<correlatedFeatures> SadGetNormalModel(SimpleAnomalyDetector * s) {
+	return s->getNormalModel();
+}
+extern "C" __declspec(dllexport) void SadSetCorrelationThreshold(SimpleAnomalyDetector* s, float threshold) {
+	s->setCorrelationThreshold(threshold);
+}
+
+/*
+* HybridAnomalyDetector (Actually only finds circles)
+*/
+extern "C" __declspec(dllexport) void* createHad() {
+	return (void*) new HybridAnomalyDetector();
+}
+extern "C" __declspec(dllexport) void HadLearnNormal(HybridAnomalyDetector * h, const TimeSeries & ts) {
+	h->learnNormal(ts);
+}
+extern "C" __declspec(dllexport) vector<AnomalyReport> HadDetect(SimpleAnomalyDetector * h, const TimeSeries & ts) {
+	return h->detect(ts);
+}
+
+
