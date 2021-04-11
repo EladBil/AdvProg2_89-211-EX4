496d74bce212500477be89820febb9807a69f3b8
Yedidya Bachar <72942868+yedidya3@users.noreply.github.com>
2021-04-11 10:14:32 +0300

    Update IModel.cs

--- IModel.cs
+++ IModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
 
-namespace model
+namespace FlightSimADVProg2_ex1.Model
 {
     interface IModel : INotifyPropertyChanged
     {
